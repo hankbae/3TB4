185203d57b2645e9e9add5aee45bcd5ecbf78da7
Hank Bae <baeh2@mcmaster.ca>
2019-02-06 08:59:36 -0500

    prelab2 game still unfinished

---
+++ Lab2_hank/actual_lab/clock_divider.v
@@ -0,0 +1,16 @@
+module clock_divider(
+	input Clock, Reset_n,
+	input [20:0] divisor,
+	output clk_ms
+	);
+	
+	reg [20:0] cntr;
+	
+	always @(posedge Clock)
+	begin
+		if ((Reset_n==1'b0)||(cntr >= (divisor-1'b1))) cntr[20:0] <= 21'd0;
+		else cntr[20:0] <= cntr[20:0] + 21'd1;
+	end
+	assign clk_ms = (cntr[20:0]>=(divisor/2))?1'b1:1'b0;
+	
+endmodule
