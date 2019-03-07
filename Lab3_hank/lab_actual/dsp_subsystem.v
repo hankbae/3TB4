cc17389ff9b4d08be669bc713533ed75b8260cff
Hank Bae <baeh2@mcmaster.ca>
2019-03-06 23:23:39 -0500

    created lab_actual for lab3

---
+++ Lab3_hank/lab_actual/dsp_subsystem.v
@@ -0,0 +1,46 @@
+module dsp_subsystem (
+	input sample_clock,
+	input reset,
+	input [1:0] selector,
+	input [15:0] input_sample,
+	output reg [15:0] output_sample
+	);
+
+//assign output_sample = input_sample;
+
+wire [15:0] FIR_out;
+wire [15:0] ECHO_out;
+
+
+FIR50 (
+	.clk(sample_clock),
+	.signal_in(input_sample),
+	.taps(8'd161),
+	.signal_out(FIR_out)
+	);
+	
+shiftregister (
+	.clock(sample_clock),
+	.shiftin(input_sample),
+	.shiftout(ECHO_out),
+	.taps()
+	);
+
+always @(posedge sample_clock or posedge reset)
+begin
+	if (reset == 1'b1) output_sample <= input_sample;
+	else
+	begin
+		case (selector)
+			2'b0:  output_sample <= input_sample;	// straight passthrough
+			
+			2'b1:	 output_sample <= FIR_out;			// FIR filter
+			
+			2'b10: output_sample <= input_sample + ECHO_out;		// Echo machine
+			
+			default: output_sample <= input_sample; // straight passthrough 
+		endcase
+	end
+end
+
+endmodule
