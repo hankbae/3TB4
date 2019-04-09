3daa269b3767f97984c90443a6838e4f99b2a0ab
Hank Bae <baeh2@mcmaster.ca>
2019-04-09 13:20:02 -0400

    compiler automated but lab5 not working

--- Lab5_hank/tutorial/compiler.py
+++ Lab5_hank/tutorial/compiler.py
@@ -1,5 +1,5 @@
 def num2bin(val,bits):
-    if val > 0:
+    if val >= 0:
         tmp = bin(val)
         return tmp[2:]
     else:
@@ -23,34 +23,34 @@ def reg2bin(string):
 def line2bin(line):
     if line[0] == "BR":
         tmp = num2bin(int(line[1]),5)
-        writeline = "100" + (5-len(tmp)) * "0"+tmp +"\n"
+        writeline = "100" + (5-len(tmp)) * "0"+tmp
     elif line[0] == "BRZ":
         tmp = num2bin(int(line[1]),5)
-        writeline = "101" + (5-len(tmp))*"0" + tmp + "\n"
+        writeline = "101" + (5-len(tmp))*"0" + tmp
     elif line[0] == "ADDI":
         tmp = num2bin(int(line[2]),3)
-        writeline = "000" + (3-len(tmp))*"0" + tmp + reg2bin(line[1]) + "\n"
+        writeline = "000" + (3-len(tmp))*"0" + tmp + reg2bin(line[1])
     elif line[0] == "SUBI":
         tmp = num2bin(int(line[2]),3)
-        writeline = "001" + (3-len(tmp))*"0" + tmp + reg2bin(line[1]) + "\n"
+        writeline = "001" + (3-len(tmp))*"0" + tmp + reg2bin(line[1])
     elif line[0] == "SR0":
         tmp = num2bin(int(line[1]),4)
-        writeline = "0100" + (4-len(tmp))*"0" + tmp + "\n"
+        writeline = "0100" + (4-len(tmp))*"0" + tmp
     elif line[0] == "SRH0":
         tmp = num2bin(int(line[1]),4)
-        writeline = "0101" + (4-len(tmp))*"0" + tmp + "\n"
+        writeline = "0101" + (4-len(tmp))*"0" + tmp
     elif line[0] == "CLR":
-        writeline = "011000"+ reg2bin(line[1]) + "\n"
+        writeline = "011000"+ reg2bin(line[1])
     elif line[0] == "MOV":
-        writeline = "0111" + reg2bin(line[1]) + reg2bin(line[2]) + "\n"
+        writeline = "0111" + reg2bin(line[1]) + reg2bin(line[2])
     elif line[0] == "MOVA":
-        writeline = "110000"+ reg2bin(line[1]) + "\n"
+        writeline = "110000"+ reg2bin(line[1])
     elif line[0] == "MOVR":
-        writeline = "110001"+ reg2bin(line[1]) + "\n"
+        writeline = "110001"+ reg2bin(line[1])
     elif line[0] == "MOVRHS":
-        writeline = "110010"+ reg2bin(line[1]) + "\n"
+        writeline = "110010"+ reg2bin(line[1])
     elif line[0] == "PAUSE":
-        writeline = 8*"1"+"\n"
+        writeline = 8*"1"
     else:
         writeline = ""
     return(writeline)
@@ -71,15 +71,40 @@ def str2line(line):
 def main():
     filename = input("file name:")
     infile = open(filename,"r")
-    outfile = open(filename[:-4]+"_out.txt","w")
-    line = 1
-    while line != ['']:
+    outfile = open("instruction_rom.mif","w")
+    ## header of file
+    outfile.write("-- Copyright (C) 2017  Intel Corporation. All rights reserved.\n")
+    outfile.write("-- Your use of Intel Corporation's design tools, logic functions \n")
+    outfile.write("-- and other software and tools, and its AMPP partner logic \n")
+    outfile.write("-- functions, and any output files from any of the foregoing \n")
+    outfile.write("-- (including device programming or simulation files), and any \n")
+    outfile.write("-- associated documentation or information are expressly subject \n")
+    outfile.write("-- to the terms and conditions of the Intel Program License \n")
+    outfile.write("-- Subscription Agreement, the Intel Quartus Prime License Agreement,\n")
+    outfile.write("-- the Intel FPGA IP License Agreement, or other applicable license\n")
+    outfile.write("-- agreement, including, without limitation, that your use is for\n")
+    outfile.write("-- the sole purpose of programming logic devices manufactured by\n")
+    outfile.write("-- Intel and sold by Intel or its authorized distributors.  Please\n")
+    outfile.write("-- refer to the applicable agreement for further details.\n\n")
+    outfile.write("-- Quartus Prime generated Memory Initialization File (.mif)\n\n")
+    outfile.write("WIDTH=8;\nDEPTH=256;\n\n")
+    outfile.write("ADDRESS_RADIX=UNS;\nDATA_RADIX=BIN;\n\n")
+    outfile.write("CONTENT BEGIN\n")
+    outfile.write("\t0    :   00000000;\n")
+    # content
+    line = infile.readline()
+    cntr = 1
+    while line != '':
+        if line[0] != "#":
+            line = str2line(line)
+            print(line)
+            writeline = line2bin(line)
+            print(writeline)
+            outfile.write("\t"+str(cntr)+(5-len(str(cntr)))*" "+":   "+writeline+";\n")
+            cntr+=1
         line = infile.readline()
-        line = str2line(line)
-        print(line)
-        writeline = line2bin(line)
-        print(writeline)
-        outfile.write(writeline)
+    outfile.write("\t["+str(cntr)+"..255]  :   00000000;\n")
+    outfile.write("END;\n")
     outfile.close()
     infile.close()
 
--- Lab5_hank/tutorial/simulation/qsim/lab5.do
+++ Lab5_hank/tutorial/simulation/qsim/lab5.do
@@ -1,7 +1,7 @@
 onerror {exit -code 1}
 vlib work
 vlog -work work lab5.vo
-vlog -work work datapath.vwf.vt
+vlog -work work lab5.vwf.vt
 vsim -novopt -c -t 1ps -L cyclonev_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate_ver -L altera_lnsim_ver work.datapath_vlg_vec_tst
 vcd file -direction lab5.msim.vcd
 vcd add -internal datapath_vlg_vec_tst/*
@@ -15,3 +15,6 @@ proc simTimestamp {} {
 after 2500 simTimestamp
 run -all
 quit -f
+
+
+
